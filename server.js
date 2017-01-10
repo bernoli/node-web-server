d3c545d556788b5074656e68b4cac57ecf7e336f
Yoav Kaplan <yoav.kaplan@gmail.com>
2017-01-10 23:43:21 +0200

    adding public folder, use in express

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express(); // cerating express app
-
-app.get('/', function(req, res)
-{
-	res.send('hello express');
-});
-
+var PORT = 3000;
 app.get('/about', function(req, res){
 	res.send('about us');
 });
 
-app.listen(3000);
+app.use(express.static(__dirname+"/public"));
+
+app.listen(port, function(){
+	console.log("express server is listen on port: " + PORT);
+});
