14dccfa6cbbb2cb95b889838bc921cba441c1691
Yoav Kaplan <yoav.kaplan@gmail.com>
2017-01-11 06:33:19 +0200

    refactor middleware to its own module

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express');
+var middleware = require('./middleware').middleware;
 var app = express(); // cerating express app
 var PORT = 3000;
 
 // middlewares are like filters in asp.net? you add them at a chain in a pipeline previous to the page request
 // this way we can add cross cutting behaviors to routes level or at global for exmaple we can log any request. 
-var middleware = 
-{
-	requireAuthentication:function(req,res, next)
-	{
-		console.log("private route hit");
-		next();
-	},
-	logger:function(req,res, next){
-		console.log(req.method+", original url:" + req.originalUrl);
-		next();
-	}
-}
+
 
 // app level middleware, gets called for any page request
 app.use(middleware.logger);
